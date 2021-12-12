c8c7c06e0c887660c15523cbd40f7b4dc70bcb72
hope <hope@gravylab.co.kr>
2021-12-12 10:38:09 +0900

    Initial commit

---
+++ PanGestureApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PanGestureApp
+//
+//  Created by Hope on 2021/12/12.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
